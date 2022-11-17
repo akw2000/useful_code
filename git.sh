ce60f224a95298ab10095e6407c85754403cd963
Kavindu Warnakulasuriya <86293207+akw2000@users.noreply.github.com>
2022-11-17 14:43:55 +0530

    Create git.sh

---
+++ git.sh
@@ -0,0 +1,26 @@
+// run from the directory 
+git init 
+// which branch 
+git status 
+git add . 
+git commit -m "add" 
+git push origin main 
+// personal access token 
+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
+
+// public repo clone just allow hidden files and delete .git file init a repo through vscode
+
+reverting to a specific commit 
+git log --oneline 
+// grab the address + don’t forget the dot 
+git checkout be9055b . git add -A 
+git commit -m "custom message" g
+it push
+
+// resolve conflicts
+// go to main and pull
+git rebase main
+git rebase --continue
+git push --force
+
+//always create a branch and work and commit for pull request
