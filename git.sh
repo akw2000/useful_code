e20beb7886aabe94142832159275a67b2808d3a6
Kavindu Warnakulasuriya <86293207+akw2000@users.noreply.github.com>
2022-12-03 18:19:14 +0530

    added how to rebase, remove specific commit

--- git.sh
+++ git.sh
@@ -1,25 +1,33 @@
-// run from the directory 
+# // run from the directory 
 git init 
-// which branch 
+# // which branch 
 git status 
 git add . 
 git commit -m "add" 
 git push origin main 
-// personal access token 
+# // personal access token 
 
-// public repo clone just allow hidden files and delete .git file init a repo through vscode
+# // public repo clone just allow hidden files and delete .git file init a repo through vscode
 
-reverting to a specific commit 
+# reverting to a specific commit 
 git log --oneline 
-// grab the address + don’t forget the dot 
+# // grab the address + don’t forget the dot 
 git checkout be9055b . git add -A 
 git commit -m "custom message" g
 it push
 
-// resolve conflicts
-// go to main and pull
+# // resolve conflicts
+# // go to main and pull
 git rebase main
 git rebase --continue
 git push --force
 
-//always create a branch and work and commit for pull request
+# //always create a branch and work and commit for pull request
+
+// remove specific commit
+git rebase -i bl8976t
+
+# When you execute the command an editor will open with a text similar to this:
+# Just go on and delete the lines you don\'t want, like this:
+# Save the file and close the editor.
+git push -f
